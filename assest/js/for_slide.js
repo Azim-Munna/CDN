4c9f952814c903bdb666e449e3acceb07628a0fb
Azim Munna <31302195+Azim-Munna@users.noreply.github.com>
2019-03-11 19:40:02 +0600

    for slide
    dkjf

---
+++ assest/js/for_slide.js
@@ -0,0 +1,58 @@
+(function($) {
+    "use strict"
+// magnificPopup
+    $('.video_icon_btn').magnificPopup({
+        type: 'iframe'
+
+    });
+    // owl
+    $(".client_review_slider_area").owlCarousel({
+        items: 1,
+        loop: true,
+        autoplay: true,
+        margin: 20,
+        autoplaySpeed: 2000,
+        nav: true,
+        dots: true,
+        smartSpeed: 2000,
+        navText: ['<i class="fas fa-arrow-left"></i>', '<i class="fas fa-arrow-right"></i>'],
+        responsive: {
+            0: {
+                items: 1,
+                autoplay: true
+            },
+            480: {
+                items: 1
+            },
+            768: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            },
+            1200: {
+                items: 1
+            }
+        }
+    });
+    $(".owl-loaded ").addClass("owl-carousel");
+
+})(jQuery);
+/*
+jQuery(document).ready(function($) {
+
+    var menuTrigger = "<span class='caret caret-up'></span>";
+
+    var hasSubmenu = $('ul li').has('ul').addClass('hasSubmenu').find('>a').prepend(menuTrigger);
+
+    $('.caret').on('click', function() {
+
+        $(this).toggleClass('caret-down').closest('li').siblings().find('.caret').removeClass('caret-down');
+
+        $(this).closest('li').find('>ul').stop(true, true).slideToggle();
+
+
+    });
+    
+});
+*/
